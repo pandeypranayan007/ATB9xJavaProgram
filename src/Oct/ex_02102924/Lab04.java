05545906edd74fe5aed0c2df7b1745bf7a3f3b4d
pandeypranayan007 <pandeypranayan@gmail.com>
2024-10-09 15:04:31 +0530

    Fourth Commit

---
+++ src/Oct/ex_02102924/Lab04.java
@@ -0,0 +1,14 @@
+//Print the table using the printf() command.
+
+package Oct.ex_02102924;
+
+public class Lab04 {
+      public static void main(String[] args){
+          int x=10;
+          int i;
+          for(i=1; i<=x; i++){
+              System.out.printf(x+" * "+i+" => "+ (x*i));
+              System.out.println();
+          }
+      }
+}
