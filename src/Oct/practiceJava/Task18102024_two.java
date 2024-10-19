e3800933cd3a56fdc3070f86bb7267ccd7ce4638
pandeypranayan007 <pandeypranayan@gmail.com>
2024-10-19 12:52:49 +0530

    Eight commit

---
+++ src/Oct/practiceJava/Task18102024_two.java
@@ -0,0 +1,52 @@
+package Oct.practiceJava;
+
+import java.util.Scanner;
+
+public class Task18102024_two {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Month :");
+        int num= sc.nextInt();
+        switch(num){
+            case 1:
+                System.out.println("Jan");
+                break;
+            case 2:
+                System.out.println("Feb");
+                break;
+            case 3:
+                System.out.println("mar");
+                break;
+            case 4:
+                System.out.println("april");
+                break;
+            case 5:
+                System.out.println("may");
+                break;
+            case 6:
+                System.out.println("june");
+                break;
+            case 7:
+                System.out.println("july");
+                break;
+            case 8:
+                System.out.println("aug");
+                break;
+            case 9:
+                System.out.println("sep");
+                break;
+            case 10:
+                System.out.println("oct");
+                break;
+            case 11:
+                System.out.println("nov");
+                break;
+            case 12:
+                System.out.println("dec");
+                break;
+            default:
+                System.out.println("Error");
+        }
+
+    }
+}
