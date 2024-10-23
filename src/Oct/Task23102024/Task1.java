f768a93f7d764a33fd3533c016b989c68291c657
pandeypranayan007 <pandeypranayan@gmail.com>
2024-10-23 19:47:10 +0530

    Put Double in argument

--- src/Oct/Task23102024/Task1.java
+++ src/Oct/Task23102024/Task1.java
@@ -12,7 +12,7 @@ public class Task1 {
     static int mul(int num1, int num2){
         return num1*num2;
     }
-    static double  div(int num1,int num2){
+    static double div(double num1,double num2){
         return num1/num2;
     }
 
